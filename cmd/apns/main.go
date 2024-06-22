5df186ced6ca35155dde281df7f323c45869ade5
Øyvind Hauge <oyvind.s.hauge@gmail.com>
2024-06-22 20:50:59 +0200

    ...

--- cmd/apns/main.go
+++ cmd/apns/main.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 const (
+	// You can also use port 2197 (instead of port 443) on either server when communicating with APNs.
+	//You might use this port to allow APNs traffic through your firewall but to block other HTTPS traffic.
 	apnsDev  = "api.sandbox.push.apple.com:443"
 	apnsProd = "api.push.apple.com:443"
 )
 
 func main() {
+
+	// https://developer.apple.com/documentation/usernotifications/sending-notification-requests-to-apns
 	fmt.Println("Hello, APNS!")
 }
